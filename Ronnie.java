65308e64525407c184653a167c430126212d6a53
SrAce <gabriel.alons3@gmail.com>
2016-07-21 19:46:18 -0300

    addJOptionPane

--- Ronnie.java
+++ Ronnie.java
@@ -1,8 +1,11 @@
+
+import javax.swing.JOptionPane;  
 public class Porcentagem{
 	public static void main(String [ ] args){
 
 		float valor, dinheiros;
-		
+		valor = Float.parseFloat(JOptionPane.showInputDialog(null,"R$ Valor do produto",JOptionPane.QUESTION_MESSAGE));  
+        dinheiros = Integer.parseInt(JOptionPane.showInputDialog(null,"Taxa do aumento",JOptionPane.QUESTION_MESSAGE));;
 
 
 
