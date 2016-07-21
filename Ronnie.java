de3886f9a2a28be8ee2a1448d4958558075e8266
SrAce <gabriel.alons3@gmail.com>
2016-07-21 19:54:55 -0300

    quase.pronto.com.fomrula

--- Ronnie.java
+++ Ronnie.java
@@ -3,10 +3,11 @@ import javax.swing.JOptionPane;
 public class Porcentagem{
 	public static void main(String [ ] args){
 
-		float valor, dinheiros;
-		valor = Float.parseFloat(JOptionPane.showInputDialog(null,"R$ Valor do produto",JOptionPane.QUESTION_MESSAGE));  
-        dinheiros = Integer.parseInt(JOptionPane.showInputDialog(null,"Taxa do aumento",JOptionPane.QUESTION_MESSAGE));;
-
+		float valor, dinheiros, formula;
+		valor = Float.parseFloat(JOptionPane.showInputDialog(null,"R$ Valor do produto"));  
+        dinheiros = Float.parseFloat(JOptionPane.showInputDialog(null,"Juros"));
+        formula = valor*(1+(dinheiros/100));
+        JOptionPane.showMessageDialog("Dinheiros final = "+formula" com "+dinheiros"% de juros.");
 
 
 	}
